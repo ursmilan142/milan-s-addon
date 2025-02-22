e80813d02d8b0cd3dd87d7be1169bc5407558c8b
ursmilan142 <42483146+ursmilan142@users.noreply.github.com>
2025-02-23 00:59:55 +1100

    Update app.py

--- app.py
+++ app.py
@@ -239,4 +239,4 @@ def serve_logo():
         return "Logo not found", 404
         
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8001, debug=False)
