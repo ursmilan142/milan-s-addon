2449374570f75492527ecee566cc5499f9f142bd
ursmilan142 <42483146+ursmilan142@users.noreply.github.com>
2025-02-23 01:47:52 +1100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def manifest():
         "resources": ["stream"],
         "types": ["movie", "series"],
         "idPrefixes": ["tt"],  # Support IMDb IDs
-        "logo": "https://github.com/ursmilan142/milan-s-addon/blob/f33720bbaf4e1d4cb07cdb3c9119abc08038c91f/logo.jpg"
+        "logo": f"https://{request.host}/logo.jpg"
     })
 
 @app.route('/stream/<type>/<id>.json')
